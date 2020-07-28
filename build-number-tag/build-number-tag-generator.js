bf279c464f57c652902406f79bd90e90bed61d27
dulceonejs <DulceOnejs@gmail.com>
2020-07-28 11:47:01 +0300

    Test example comand

--- build-number-tag/build-number-tag-generator.js
+++
@@ -1,7 +0,0 @@
-const branch = require('git-branch');
-const fs = require('fs');
-
-const dt = new Date().toGMTString();
-const buildNumber = `build number: ${branch.sync()}-branch, ${dt}`;
-
-fs.writeFileSync('./build-number', buildNumber);
