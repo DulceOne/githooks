9563bb0de443b0e1e8ad76f92ca1665fdb221384
dulceonejs <DulceOnejs@gmail.com>
2020-07-28 15:28:50 +0300

    Fix path

---
+++ .githooks/pre-commit/pre-commit/build-number-tag-generator.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+const branch = require('git-branch');
+const fs = require('fs');
+const uid = require('uuid');
+
+const buildNumber = `${branch.sync()}${uid()}`;
+fs.writeFileSync('./build-number', buildNumber);
